eb52c3fcd8608055ef331d6e6ea756b1ef0d2bbf
huyanxiang <huyx@seari.com>
2026-05-26 09:00:28 +0800

    无用按钮去除

--- src/components/dify-chatbot/DifyApiDialog.vue
+++ src/components/dify-chatbot/DifyApiDialog.vue
@@ -80,7 +80,7 @@
         <el-button type="warning" @click="clearMessages">清空对话</el-button>
         <el-button type="primary" @click="outputJsonToConsole">AI生成画布</el-button>
         <el-button type="success" @click="saveRawJson">原始保存</el-button>
-        <el-button type="info" @click="saveTempPayload">临时保存payload</el-button>
+        <!-- <el-button type="info" @click="saveTempPayload">临时保存payload</el-button> -->
         <!-- <el-button type="success" @click="fetchAndSaveScreenAI">从URL读取JSON</el-button> -->
         <el-button type="danger" @click="calibrateJson">校准JSON</el-button>
         <el-button type="primary" @click="copyLastMessageContent">复制回答内容</el-button>
