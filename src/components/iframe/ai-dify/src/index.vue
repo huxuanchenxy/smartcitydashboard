eab4661e84a0aad8c07b27f66435ef35ef8ab20f
huyanxiang <huyx@seari.com>
2026-06-15 13:44:16 +0800

    水务弹出对话框ai

--- src/components/iframe/ai-dify/src/index.vue
+++ src/components/iframe/ai-dify/src/index.vue
@@ -3,17 +3,28 @@
     <button :style="buttonStyle" @click="handleClick">
       <img :src="buttonImage" style="height: 40px; width: 40px;">
     </button>
+    
+    <!-- Dify API 聊天机器人弹框 -->
+    <DifyApiDialog
+      v-model:visible="difyApiDialogVisible"
+      :data="difyData"
+      @close="handleDifyApiDialogClose"
+      @message-received="handleDifyApiMessageReceived"
+      @message-sent="handleDifyApiMessageSent"
+    />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, computed, toRef } from 'vue'
+import { defineComponent, PropType, computed, toRef, ref } from 'vue'
 import type { CSSProperties } from 'vue'
 import { AiDify } from './ai-dify'
 import { useDataCenter } from '@/mixins/data-center'
+import DifyApiDialog from '@/components/dify-chatbot/DifyApiDialog.vue'
 
 export default defineComponent({
   name: 'VAiDify',
+  components: { DifyApiDialog },
   props: {
     com: {
       type: Object as PropType<AiDify>,
@@ -27,6 +38,10 @@ export default defineComponent({
     const config = toRef(props.com, 'config')
     const attr = toRef(props.com, 'attr')
     const comid = toRef(props.com, 'id').value
+    
+    // Dify API 聊天机器人相关状态
+    const difyApiDialogVisible = ref(false)
+    const difyData = ref({})
 
     const buttonImage = computed(() => {
       return config.value.buttonImage
@@ -34,6 +49,19 @@ export default defineComponent({
 
     const handleClick = () => {
       console.log('AiDify button clicked')
+      difyApiDialogVisible.value = true
+    }
+    
+    const handleDifyApiDialogClose = () => {
+      console.log('Dify API dialog closed')
+    }
+    
+    const handleDifyApiMessageReceived = () => {
+      console.log('Dify API message received')
+    }
+    
+    const handleDifyApiMessageSent = () => {
+      console.log('Dify API message sent')
     }
 
     const buttonStyle = computed(() => {
@@ -65,6 +93,11 @@ export default defineComponent({
       wrapperStyle,
       handleClick,
       comid,
+      difyApiDialogVisible,
+      difyData,
+      handleDifyApiDialogClose,
+      handleDifyApiMessageReceived,
+      handleDifyApiMessageSent,
     }
   },
 })
